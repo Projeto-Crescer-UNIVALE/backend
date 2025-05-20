94bd276466e93fa695ee007d55b43dd849518ade
jeiel2013 <contatojeiel2013@gmail.com>
2025-05-20 18:58:32 -0300

    chore: dependecias e arquivos prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/PrismaService.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/require-await */
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    process.on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
