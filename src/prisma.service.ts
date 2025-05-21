d4ac0f5e011662fac7a9ad8d5c4085d61b4d64e6
jeiel2013 <contatojeiel2013@gmail.com>
2025-05-20 22:12:11 -0300

    chore: consertando arquivos

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,18 +1,16 @@
-/* eslint-disable @typescript-eslint/require-await */
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$connect();
   }
 
   async enableShutdownHooks(app: INestApplication) {
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
     process.on('beforeExit', async () => {
       await app.close();
     });
   }
 }
+// Os erros que estão dando não atrapalham em nada, o projeto roda normal mesmo com eles.
